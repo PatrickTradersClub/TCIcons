338456e77fa64b213c36dfa3e579f9125462393f
Patrick Chagas <patrick.chagas@tradersclub.com.br>
2020-11-20 19:28:15 -0300

    fim

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./svg/index";
 export { default as TCIcon } from "./components/TCIcon";
-export { default as Fim } from "./components/Fim";
+export { default as FimOne } from "../src/components/Fim";
+export { default as FimTwo } from "./components/Fim";
 export { default as wrapIcon } from "./utils/wrapIcon";
