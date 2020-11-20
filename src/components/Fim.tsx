144e46e3b5c89bca1d13e0d457096b9525659270
Patrick Chagas <patrick.chagas@tradersclub.com.br>
2020-11-20 19:15:23 -0300

    fim

---
+++ src/components/Fim.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+// import { Container } from './styles';
+
+const Fim: React.FC = () => {
+  return (
+    <View style={{ backgroundColor: "#afa" }}>
+      <Text>Agora vai</Text>
+    </View>
+  );
+};
+
+export default Fim;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from  './svg/index';
-export {default as TCIcon} from './components/TCIcon';
-export {default as wrapIcon} from './utils/wrapIcon';
+export * from "./svg/index";
+export { default as TCIcon } from "./components/TCIcon";
+export { default as Fim } from "./components/Fim";
+export { default as wrapIcon } from "./utils/wrapIcon";
