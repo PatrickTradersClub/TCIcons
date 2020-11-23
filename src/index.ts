aa0190b54906e986aec9c1e7c04d1bb1dab209ab
Arthur Geron <arthurgeron@MacBook-Pro-de-Mac.local>
2020-11-22 21:03:24 -0300

    fix(icon): cleaned up

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export { default as TCSvg } from "./components/TCSvg";
-export { default as wrapIcon } from "./utils/wrapIcon";
-export * from "./components/Svgs";
 export * from "./svg";
--- src/utils/wrapIcon.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ComponentType, Suspense, ReactElement } from 'react';
-import TCIcon from '../components/TCSvg';
-
-export default function wrapIcon<TProps>(icon: string): ReactElement {
-  return (<TCIcon icon={icon} />);
-}
