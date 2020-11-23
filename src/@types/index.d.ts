70ca461e3646a510045f214bb81faac37bd791a5
Arthur Geron <arthurgeron@MacBook-Pro-de-Mac.local>
2020-11-22 22:08:48 -0300

    fix(icon): fixed type

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,9 +1,4 @@
-interface TCSvgProps extends SvgProps {
-    fill: string;
-}
-
 declare module "*.svg" {
-    import { SvgProps } from "react-native-svg";
-    const content: React.FC<TCSvgProps>;
+    const content: string;
     export default content;
 }
